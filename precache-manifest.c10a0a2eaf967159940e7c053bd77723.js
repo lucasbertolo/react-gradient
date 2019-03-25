d868c7b935b9dac1e5e4fbd313832aefc81661ce
lucasbertolo <lucasbertolo@github.io>
2019-03-25 02:24:37 -0300

    Updates

---
+++ precache-manifest.c10a0a2eaf967159940e7c053bd77723.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gradient/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "5d6ac5beee27c1ac9641",
+    "url": "/react-gradient/static/css/main.de2070cc.chunk.css"
+  },
+  {
+    "revision": "fac17f5064e1aac1e393",
+    "url": "/react-gradient/static/js/runtime~main.9bf57dcc.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gradient/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e3ad5edcf6e416d17d3b",
+    "url": "/react-gradient/static/js/2.393f19e5.chunk.js"
+  },
+  {
+    "revision": "5d6ac5beee27c1ac9641",
+    "url": "/react-gradient/static/js/main.565bcab5.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gradient/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gradient/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gradient/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "e3ad5edcf6e416d17d3b",
+    "url": "/react-gradient/static/css/2.e12512be.chunk.css"
+  },
+  {
+    "revision": "c5e5a9aad9a7fc4b1332f5371dda5c87",
+    "url": "/react-gradient/index.html"
+  }
+];
